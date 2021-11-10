f706a8307415ef2c37dbe1e6997d7da90ea9c124
Rohan Tomar <rstomar710@gmail.com>
2021-11-10 11:50:06 +0530

    project initialized

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ReviewDetails from './screens/ReviewDetails';
+import Home from './screens/Home';
+
+import Headers from './screens/Header';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Headers/>
+      <Home/>
+      <ReviewDetails/>
+      
     </View>
   );
 }
---
+++ screens/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {View, StyleSheet, Text} from "react-native"
+
+
+export default function Headers(){
+    return(
+        <View>
+<Text>This is header</Text>
+
+
+        </View>
+
+
+
+    )
+}
+
+
+const styles=StyleSheet.create({
+
+})
